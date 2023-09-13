14b4c6e3679645b1d5a394b10332586672ae9e6e
p24jmorris <144147868+JonMorris8@users.noreply.github.com>
2023-09-13 09:13:02 -0400

    finished 1-2

--- file.py
+++ file.py
@@ -1,17 +1,15 @@
+# Dylan: CKL 09/12/22
+
 # 1) Create a Python file named lab_1-2.py
-
 # 2) Add the heading to the first line of your file.
-
 # 3) Create a variable and set it equal to your first name. Make sure the variable follows all the variable naming rules.
-
+x = "Dylan"
 # 4) Create a print statement to display your variable when your code is run.
-
+print (x)
 # 5) Run the program.
-
 # 6) Hit enter twice. Then hit Tab once or Space four times.
-
 # 7) Create a variable and set it equal to "your favorite hobby". Make sure the variable follows all the variable naming rules.
-
+y = "Video games"
 # 8) Create a print statement to display your variable when your code is run.
-
+print (y)
 # 9) What happens when you run the program again?
